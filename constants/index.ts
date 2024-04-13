27ee0e75097d1748e5948cbb910936224b3f26cd
notAnii <animazingtoho@gmail.com>
2024-04-13 15:28:05 +0400

    feat(Contact): add contact section

--- constants/index.ts
+++ constants/index.ts
@@ -3,6 +3,6 @@ export const NAV_LINKS = [
     { href: '/about', key: 'about_me', label: 'About Me' },
     { href: '/portfolio', key: 'portfolio', label: 'Portfolio' },
     { href: '/', key: 'albums ', label: 'Albums' },
-    { href: '/', key: 'contact', label: 'Contact' },
+    { href: '/contact', key: 'contact', label: 'Contact' },
   ];
   
