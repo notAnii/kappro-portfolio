08e2740aa1e81b747e22e24c0adfd0a1868d3a1b
notAnii <animazingtoho@gmail.com>
2024-06-12 22:47:34 +0400

    chore: code clean up

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -5,9 +5,7 @@ import MobileNav from "./MobileNav"
 import { NAV_LINKS } from "@/constants"
 import Image from "next/image"
 
-type Props = {}
-
-const Header = (props: Props) => {
+const Header = () => {
   return (
     <header className='fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center'>
         <div className="flex flex-col lg:flex-row lg:items-center w-full justify-between">
