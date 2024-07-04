534c027f4b31b46e01db84ccbf458d42cbb187fa
notAnii <animazingtoho@gmail.com>
2024-07-04 23:15:35 +0400

    fix: add meta viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import Header from "@/components/layout/Header";
 
@@ -6,6 +6,11 @@ export const metadata: Metadata = {
   title: "Kappro Portfolio | Photographer & Film Maker",
   description: "Explore the portfolio of Ahmed Osman, a photographer and film maker.",
 };
+
+export const viewport: Viewport = {
+  initialScale: 1,
+  width: 'device-width'
+}
 
 export default function RootLayout({
   children,
